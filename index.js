34163db70f48870f7d71a2a60b18f587177b5423
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 00:09:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
+function sayHiToGrandma(string) {
+  console.log('HELLO')
+  console.log('hello')
+  console.log('I love you, Grandma')
+}
